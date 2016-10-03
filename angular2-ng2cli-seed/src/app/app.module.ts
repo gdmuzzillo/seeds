c5dd9f27e81b166f6c455ac9a18c8e4a5b5444be
Gustavo D. Muzzillo <gustavo.muzzillo@redb.ee>
2016-10-03 12:26:07 -0300

    ng2-bootstrap modules registration

--- angular2-ng2cli-seed/src/app/app.module.ts
+++ angular2-ng2cli-seed/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { PaginationModule } from 'ng2-bootstrap/components/pagination';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    PaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
